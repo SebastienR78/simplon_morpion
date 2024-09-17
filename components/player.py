a1cd88a2c38b9b53b1ed295b375454e0fc4e8953
dragent <alexiscau@gmail.com>
2024-09-17 16:03:37 +0200

    Avancement du projet

--- components/player.py
+++ components/player.py
@@ -1,19 +1,30 @@
 import numpy as np
 
-# récupère
+# récupère un tableau 
+# intialise une valeur de vérification à False
+# Tant qu'elle est fausse 
+#   Le joueur nnote son choix
+#   Si la variable est dans la liste de possibilité
+#       x = le reste du choix divisé par 3
+#       y = la division du choix et de 3
+#       si cette place est vide
+#           initialise la condition de sortie à Vraix
+    
 def humanPlay(gameArray):
     emptySpace = False
     while not emptySpace:
         playerChoice = int(input("Veuillez choisir une case vide entre 1 et 9 : "))-1
         if playerChoice >=0 and playerChoice <9:
-            valuePlayer = int(playerChoice)
-            x = valuePlayer % 3
-            y =  int(valuePlayer / 3 )
+            x = playerChoice % 3
+            y =  int(playerChoice / 3 )
             if gameArray[y][x] == -1 :
                 emptySpace = True
     return (x,y)    
 
+# Enregistre la valeur du joueur à sa place dans le tableau
 def marq(place, array,player):
-    #je verifie un truc avant
-    array[place[0]][place[1]]=0
+    array[place[0]][place[1]]=player
     return array
+    
+
+    
