53e4afbf467210efe96a9121a7493da405981bb8
dragent <alexiscau@gmail.com>
2024-09-18 09:41:14 +0200

    Probleme affichage

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from components.player import humanPlay, marq, check_win
 import  components.grille as grille
 import numpy as np
-
+import matplotlib.pyplot as plt
+import time
 
 # Programme principal
+#  plt.ion() met 
 # initialise les valeurs de fin de boucle
 # initialise un tableau avec -1 pour les cases vide
 # Tant que la partie n'est pas gagnée ou qu'on n'a pas fait 9 tours
@@ -12,6 +14,7 @@ import numpy as np
 #   la grille affiche le changement
 #   Si on est à 9 lance les mêmes actions sur le joueur 2
 def game():
+    plt.ion()
     turn = 0
     win = False
     gameArray = np.array([[-1,-1,-1],[-1,-1,-1], [-1,-1,-1]])
@@ -24,8 +27,14 @@ def game():
         win = check_win(gameArray,currentPlayer)
         turn+=1
     if win:
-        print("Le joueur ",currentPlayer+1," a gagné")
+        message = "Le joueur ",currentPlayer+1," a gagné"
     else:
-        print("Il y a eu égalité")
+        message = "Il y a eu égalité"
+    plt.close()
+    grille.afficher_grille()
+    plt.text(1.5,1.5,message,fontsize=24, ha='center', va='center')
+    plt.show()
+    time.sleep(7)
+    plt.close()
 
 game()
